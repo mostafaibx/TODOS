bfcdd7b36e9f22f21ad48c9a214710abf3726c1a
Mostafa <imostafa330@gmail.com>
2023-12-22 01:10:18 +0100

    initial implementation of saving github auth user data to db

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,9 +1,37 @@
 import GithubProvider from 'next-auth/providers/github'
 import Credentials from 'next-auth/providers/credentials'
+import { PrismaClient } from '@prisma/client'
 import { NuxtAuthHandler } from '#auth'
+
+const prisma = new PrismaClient()
+
 export default NuxtAuthHandler({
   pages: {
     signIn: '/login'
+  },
+  callbacks: {
+    signIn: async ({ user }) => {
+      const userExists = await prisma.user.findMany({
+        where: {
+          email: user.email!
+        }
+      })
+      if (userExists !== []) {
+        await $fetch(
+          '/api/auth/register',
+          {
+            method: 'POST',
+            body: {
+              email: user.email!,
+              name: user.name!,
+              password: 'password'
+            }
+          }
+        )
+        return true
+      } else {
+      return true
+    }
   },
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
